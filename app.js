61ae0a5d9e39edf8c90e52e193f24ebbf29e7471
RMSeba <Romeromartin.98@hotmail.com>
2023-11-05 06:58:32 -0300

    validacion-@ytotal

--- app.js
+++ app.js
@@ -15,37 +15,58 @@ function resumen(){
     let total;
     cantidadBoletos=cantidad.value;
     descuento=categoria.value;
-    console.log(nombre)
-    console.log(apellido)
     
-    if(descuento=="estudiante"){
-        total=cantidadBoletos*200
-        totalEst=total-(total*80/100)
-        mostrador.innerHTML=`   Nombre:${mayusPrimerLetra(nombre.value)}<br>
-                                Apellido:${mayusPrimerLetra(apellido.value)}<br>
-                                Email:${mayusPrimerLetra(email.value)}<br> 
-                                Total a pagar:$${totalEst}`;
-        
+    
+    if(nombre.value==="" || apellido.value==="" || email.value===""){
+        mostrador.innerHTML="Ingrese todos los datos solicitados"
     }
-    else if(descuento=="trainee"){
-        total=cantidadBoletos*200
-        totalTrainee=total-(total*50/100)
-        mostrador.innerHTML=`   Nombre:${mayusPrimerLetra(nombre.value)}<br>
-                                Apellido:${mayusPrimerLetra(apellido.value)}<br>
-                                Email:${mayusPrimerLetra(email.value)}<br>
-                                Total a pagar:$${totalTrainee}` ;
-        
+    else if(!email.value.includes('@')){
+        mostrador.innerHTML="Ingrese un email valido"
     }
-    else {
-        total=cantidadBoletos*200
-        totalJunior=total-(total*15/100)
-        mostrador.innerHTML=`   Nombre:${mayusPrimerLetra(nombre.value)}<br>
-                                Apellido:${mayusPrimerLetra(apellido.value)}<br>
-                                Email:${mayusPrimerLetra(email.value)}<br>
-                                Total a pagar:$${totalJunior}`;;
-        
+    else{
+        if(descuento=="estudiante"){
+            total=cantidadBoletos*200
+            totalEst=total-(total*80/100)
+            mostrador.innerHTML=`   Nombre:${mayusPrimerLetra(nombre.value)}<br>
+                                    Apellido:${mayusPrimerLetra(apellido.value)}<br>
+                                    Email:${mayusPrimerLetra(email.value)}<br> 
+                                    Total a pagar:$${totalEst}`;
+            
+        }
+        else if(descuento=="trainee"){
+            total=cantidadBoletos*200
+            totalTrainee=total-(total*50/100)
+            mostrador.innerHTML=`   Nombre:${mayusPrimerLetra(nombre.value)}<br>
+                                    Apellido:${mayusPrimerLetra(apellido.value)}<br>
+                                    Email:${mayusPrimerLetra(email.value)}<br>
+                                    Total a pagar:$${totalTrainee}` ;
+            
+        }
+        else {
+            total=cantidadBoletos*200
+            totalJunior=total-(total*15/100)
+            mostrador.innerHTML=`   Nombre:${mayusPrimerLetra(nombre.value)}<br>
+                                    Apellido:${mayusPrimerLetra(apellido.value)}<br>
+                                    Email:${mayusPrimerLetra(email.value)}<br>
+                                    Total a pagar:$${totalJunior}`;;
+            
+        }
     }
-}
+    }
+    
+        
+            
+
+        
+            
+        
+    
+   
+    
+
+
+
+
 function borrar(){
     mostrador.innerHTML="Resumen:"
     tablero.style.border="none"
